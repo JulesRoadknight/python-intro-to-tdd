e1d744637479952d520e50db2099ab1b28cbef08
Jules <jroadknight@8thlight.com>
2023-10-19 13:40:45 +0100

    Refactor GildedRose

--- gilded-rose-python/gilded_rose.py
+++ gilded-rose-python/gilded_rose.py
@@ -5,35 +5,47 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+    def update_quality(self): 
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                break
+            if item.name == "Aged Brie":
+                self.increment_quality(item)
+                self.decrement_sell_in(item)
+                break
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in <= 0:
+                    item.quality = 0
+                    self.decrement_sell_in(item)
+                    break
+                elif item.sell_in <= 5:
+                    self.increment_quality(item, 3)
+                    self.decrement_sell_in(item)
+                    break
+                elif item.sell_in <= 10:
+                    self.increment_quality(item, 2)
+                    self.decrement_sell_in(item)
+                    break
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.increment_quality(item)
+                    self.decrement_sell_in(item) 
+                    break
+
+            self.decrement_quality(item)
+            self.decrement_sell_in(item)
+
+    def increment_quality(self, item, amount = 1):
+        if item.quality < 50:
+            item.quality += amount
+
+    def decrement_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+            if item.sell_in <= 0:
+               item.quality -= 1
+
+    def decrement_sell_in(self, item):
+        item.sell_in -= 1
 
 
 class Item:
--- gilded-rose-python/test_gilded_rose.py
+++ gilded-rose-python/test_gilded_rose.py
@@ -2,7 +2,7 @@ from gilded_rose import Item, GildedRose
 
 class TestGildedRose:
     def test_update_decrements_normal_sell_in(self):
-        original_sell_in = 5
+        original_sell_in = 0
         items = [Item("potion bottle", sell_in=original_sell_in, quality=5)]
         gilded_rose = GildedRose(items)
 
@@ -11,91 +11,120 @@ class TestGildedRose:
         assert items[0].sell_in == original_sell_in - 1
     
     def test_update_decrements_normal_item_quality(self):
+        original_sell_in = 10
         original_quality = 10
-        items = [Item("potion bottle", sell_in=50, quality=original_quality)]
+        items = [Item("potion bottle", sell_in=original_sell_in, quality=original_quality)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in - 1
         assert items[0].quality == original_quality - 1
 
         
     def test_item_quality_cannot_be_below_0(self):
+        original_sell_in = 14
         original_quality = 0
-        items = [Item("potion bottle", sell_in=0, quality=original_quality)]
+        items = [Item("potion bottle", sell_in=original_sell_in, quality=original_quality)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in - 1
         assert items[0].quality == original_quality
     
     def test_normal_item_quality_decrements_faster_after_sell_in(self):
+        original_sell_in = 0
         original_quality = 5
-        items = [Item("potion bottle", sell_in=0, quality=original_quality)]
+        items = [Item("potion bottle", sell_in=original_sell_in, quality=original_quality)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in - 1
         assert items[0].quality == original_quality - 2
 
     def test_aged_brie_quality_increases_over_time(self):
+        original_sell_in = 5000
         original_quality = 5
-        items = [Item("Aged Brie", sell_in=5, quality=original_quality)]
+        items = [Item("Aged Brie", sell_in=original_sell_in, quality=original_quality)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in - 1
         assert items[0].quality == original_quality + 1
 
     def test_quality_cannot_exeed_50(self):
+        original_sell_in = 42
         original_quality = 50
-        items = [Item("Aged Brie", sell_in=5, quality=original_quality)]
+        items = [Item("Aged Brie", sell_in=original_sell_in, quality=original_quality)]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in - 1
         assert items[0].quality == original_quality
 
-    def test_sulfuras_quality_does_not_change(self):
+    def test_sulfuras_properties_do_not_change(self):
+        original_sell_in = 0
+        original_sell_in = 1
         original_quality = 40
-        original_item = Item("Sulfuras, Hand of Ragnaros", sell_in=1, quality=original_quality)
+        original_item = Item("Sulfuras, Hand of Ragnaros", sell_in=original_sell_in, quality=original_quality)
         items = [original_item]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in
         assert items[0].quality == original_quality
 
     def test_concert_tickets_lose_all_quality_after_sell_in(self):
-        original_item = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=40)
+        original_sell_in = 0
+        original_item = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=original_sell_in, quality=40)
         items = [original_item]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in - 1
         assert items[0].quality == 0
 
     def test_concert_tickets_gain_quality_long_before_sell_in(self):
+        original_sell_in = 11
         original_quality = 40
-        original_item = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=original_quality)
+        original_item = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=original_sell_in, quality=original_quality)
         items = [original_item]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
+        assert items[0].sell_in == original_sell_in - 1
         assert items[0].quality == original_quality + 1
 
-    def test_concert_tickets_gain_quality_approaching_sell_in(self):
+    def test_concert_tickets_gain_quality_10_days_before_sell_in(self):
+        original_sell_in = 10
         original_quality = 10
-        original_item = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=6, quality=original_quality)
+        original_item = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=original_sell_in, quality=original_quality)
         items = [original_item]
         gilded_rose = GildedRose(items)
 
         gilded_rose.update_quality()
 
-        assert items[0].quality == original_quality + 2
+        quality_gained = 2
+        assert items[0].sell_in == original_sell_in - 1
+        assert items[0].quality == original_quality + quality_gained
 
+    def test_concert_tickets_gain_more_quality_5_days_before_sell_in(self):
+        original_sell_in = 5
+        original_quality = 10
+        original_item = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=original_sell_in, quality=original_quality)
+        items = [original_item]
+        gilded_rose = GildedRose(items)
+
+        quality_gained = 3
         gilded_rose.update_quality()
 
-        assert items[0].quality == original_quality + 5
+        assert items[0].sell_in == original_sell_in - 1
+        assert items[0].quality == original_quality + quality_gained
